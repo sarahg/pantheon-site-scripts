d9270b032929530b454ccfc17c7adf1a8731ba2c
Sarah German <sgerman@pantheon.io>
2021-01-09 14:59:08 -0600

    Add script for live deploys

--- test-and-deploy.sh
+++ test-and-deploy.sh
@@ -9,15 +9,9 @@ TAG="testing"
 ORG_UUID="1439ef14-9fed-428e-8943-902e36c763a9"
 SITES=$(terminus org:site:list --field=name --tag=${TAG} --filter="framework=wordpress" -- $ORG_UUID)
 
-# @TODO Break out the deploy/QA steps to a different script.
+# @TODO VRT Test vs Live
 
-# Prompt for a quick manual QA.
-# @TODO VRT instead! Or Autopilot!
-# @TODO Only prompt on sites we actually updated something.
-echo "[notice] Go check the Test sites: "
+# Deploy to Live
 while read -r SITENAME; do
-    echo -e "- https://test-${SITENAME}.pantheonsite.io"
+    terminus env:deploy --note="Code updates." -- "${SITENAME}".live
 done <<< "$SITES"
-
-# @TODO Deploy to Live
-#terminus env:deploy --sync-content --note="Plugin updates." -- "${SITENAME}".test
