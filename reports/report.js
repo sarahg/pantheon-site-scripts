c0f6832107b3920c85546937ae9fa678370b70d5
Sarah German <sgerman@pantheon.io>
2021-02-02 19:39:56 -0600

    Template todos

--- reports/report.js
+++ reports/report.js
@@ -1,1 +1,7 @@
 alert('hi')
+
+// @todo Render git log as a ul
+
+// @todo
+// Populate spans with corresponding values from the JSON files
+// Example: <span class="latest-backup"> pulls from {"latest-backup": "2021-10-01"}
