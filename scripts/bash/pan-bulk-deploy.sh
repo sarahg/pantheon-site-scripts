946db52828cbadb8c034706272047a977c434dea
Sarah German <sarah@sarahgerman.com>
2021-12-31 11:12:00 -0600

    Add check for pending code to deploy.

--- scripts/bash/pan-bulk-deploy.sh
+++ scripts/bash/pan-bulk-deploy.sh
@@ -10,14 +10,26 @@
 
 set -eou pipefail
 
+DEPLOYED=()
+
 for SITENAME in ${SITES//,/ }
 do
-    FRAMEWORK="$(terminus site:info "${SITENAME}" --field=Framework)"
+    # Skip this site if there isn't anything to deploy.
+    DEPLOY_STATUS=$(terminus env:deploy ${SITENAME}.live 2>&1)
+    EMPTY_RESP="nothing to deploy"
 
+    if [[ "$DEPLOY_STATUS" == *"$EMPTY_RESP"* ]]; then
+        echo "No updates to deploy for $SITENAME."
+        continue
+    fi
+
+    # Backup and deploy.
     echo "Deploying $SITENAME..."
     terminus backup:create "${SITENAME}".live --element=db
-    terminus env:deploy --note="Maintenance updates." -- "${SITENAME}".live
+    terminus env:deploy "${SITENAME}".live --note="Maintenance updates."
 
+    # CMS-specific database operations.
+    FRAMEWORK="$(terminus site:info "${SITENAME}" --field=Framework)"
     if [ "$FRAMEWORK" == "wordpress" ]; then
         terminus wp "${SITENAME}".live -- core update-db < /dev/null
     fi
@@ -26,17 +38,24 @@ do
         terminus drush "${SITENAME}".live -- updb < /dev/null
     fi
 
+    # Clear the framework cache.
+    terminus env:clear-cache "${SITENAME}".live
+
+    # Flush Redis if it's enabled.
+    # @todo reuse REDIS_CMD output
     REDIS_CMD=$(terminus connection:info ${SITENAME}.live --field="redis_command")
     if [ -n "${REDIS_CMD}" ]; then
         echo "Clearing Redis..."
-        $(terminus connection:info ${SITENAME}.live --field="redis_command") flushall
+        eval "$REDIS_CMD" flushall
     fi
 
-    terminus env:clear-cache "${SITENAME}".live
+    DEPLOYED+=($SITENAME)
 done
 
-echo "[notice] Code deployed: "
-for SITENAME in ${SITES//,/ }
-do
-    echo -e "- https://live-${SITENAME}.pantheonsite.io"
-done
+if (( ${#DEPLOYED[@]} )); then
+    echo "[notice] Code deployed: "
+    for SITENAME in ${DEPLOYED//,/ }
+    do
+        echo -e "- https://live-${SITENAME}.pantheonsite.io"
+    done
+fi
