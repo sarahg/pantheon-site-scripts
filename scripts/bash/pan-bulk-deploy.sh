0bf020072fa5bae71d91c17c1ed14249ae6b901f
Sarah German <sarah@sarahgerman.com>
2021-12-30 20:17:35 -0600

    Add Drupal 7 support.

--- scripts/bash/pan-bulk-deploy.sh
+++ scripts/bash/pan-bulk-deploy.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
-# Deploy WordPress upates to a list of sites.
+# Deploy pending code changes to a list of sites.
 #
 # This creates a database backup, and deploys
-# Test to Live. Includes update-db and a cache clear.
+# Test to Live. Includes update-db/update.php, and
+# cache clears for both the CMS and Redis.
 #
 # Usage: ./pan-bulk-deploy.sh SITES
 
 set -eou pipefail
 
-# @todo skip if there isn't anything to deploy
+# @todo abort if there isn't anything to deploy
+
 for SITENAME in ${SITES//,/ }
 do
+    FRAMEWORK="$(terminus site:info "${SITENAME}" --field=Framework)"
+
     echo "Deploying $SITENAME..."
     terminus backup:create "${SITENAME}".live --element=db
     terminus env:deploy --note="Maintenance updates." -- "${SITENAME}".live
-    terminus wp "${SITENAME}".live -- core update-db < /dev/null
+
+    if [ "$FRAMEWORK" == "wordpress" ]; then
+        terminus wp "${SITENAME}".live -- core update-db < /dev/null
+    fi
+
+    if [ "$FRAMEWORK" == "drupal" ]; then
+        terminus drush "${SITENAME}".live -- updb < /dev/null
+    fi
+
     terminus env:clear-cache "${SITENAME}".live
 done
 
-# @todo handling for cases where there are no updates.
-echo "[notice] Go check the Live sites: "
+echo "[notice] Code deployed: "
 for SITENAME in ${SITES//,/ }
 do
-    # @todo curl the homepage + login page and verify HTTP/200
     echo -e "- https://live-${SITENAME}.pantheonsite.io"
 done
