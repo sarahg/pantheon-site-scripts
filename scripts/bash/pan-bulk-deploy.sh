586f8053a3584548c2849de576adfe6dbc75e4b8
Sarah German <sarah@sarahgerman.com>
2021-12-31 11:33:01 -0600

    Add env as a parameter.

--- scripts/bash/pan-bulk-deploy.sh
+++ scripts/bash/pan-bulk-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# Deploy pending code changes to Live for a list of sites.
+# Deploy pending code changes for a list of sites.
 #
 # This creates a database backup, and deploys
 # Test to Live. Includes update-db/update.php, and
 # cache clears for both the CMS and Redis.
 #
-# Usage: ./pan-bulk-deploy.sh SITES
+# Usage: ./pan-bulk-deploy.sh SITES ENV
 
 set -eou pipefail
 
@@ -15,7 +15,7 @@ DEPLOYED=()
 for SITENAME in ${SITES//,/ }
 do
     # Skip this site if there isn't anything to deploy.
-    DEPLOY_STATUS=$(terminus env:deploy "${SITENAME}".live 2>&1)
+    DEPLOY_STATUS=$(terminus env:deploy "${SITENAME}"."${ENV}" 2>&1)
     EMPTY_RESP="nothing to deploy"
 
     if [[ "$DEPLOY_STATUS" == *"$EMPTY_RESP"* ]]; then
@@ -25,24 +25,24 @@ do
 
     # Backup and deploy.
     echo "Deploying $SITENAME..."
-    terminus backup:create "${SITENAME}".live --element=db
-    terminus env:deploy "${SITENAME}".live --note="Maintenance updates."
+    terminus backup:create "${SITENAME}"."${ENV}" --element=db
+    terminus env:deploy "${SITENAME}"."${ENV}" --note="Maintenance updates."
 
     # CMS-specific database operations.
     FRAMEWORK="$(terminus site:info "${SITENAME}" --field=Framework)"
     if [ "$FRAMEWORK" == "wordpress" ]; then
-        terminus wp "${SITENAME}".live -- core update-db < /dev/null
+        terminus wp "${SITENAME}"."${ENV}" -- core update-db < /dev/null
     fi
 
     if [ "$FRAMEWORK" == "drupal" ]; then
-        terminus drush "${SITENAME}".live -- updb < /dev/null
+        terminus drush "${SITENAME}"."${ENV}" -- updb < /dev/null
     fi
 
     # Clear the framework cache.
-    terminus env:clear-cache "${SITENAME}".live
+    terminus env:clear-cache "${SITENAME}"."${ENV}"
 
     # Flush Redis if it's enabled.
-    REDIS_CMD=$(terminus connection:info "${SITENAME}".live --field="redis_command")
+    REDIS_CMD=$(terminus connection:info "${SITENAME}"."${ENV}" --field="redis_command")
     if [ -n "${REDIS_CMD}" ]; then
         echo "Clearing Redis..."
         eval "$REDIS_CMD --no-auth-warning" flushall
@@ -55,6 +55,6 @@ if (( ${#DEPLOYED[@]} )); then
     echo "[notice] Code deployed: "
     for SITENAME in ${DEPLOYED//,/ }
     do
-        echo -e "- https://live-${SITENAME}.pantheonsite.io"
+        echo -e "- https://${ENV}-${SITENAME}.pantheonsite.io"
     done
 fi
