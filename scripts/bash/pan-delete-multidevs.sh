ed850616f8f390020f7fa4593e9b1f90d91d449e
Sarah German <sarah@sarahgerman.com>
2022-02-02 14:40:14 -0600

    Add a script to remove multidev envs.

---
+++ scripts/bash/pan-delete-multidevs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Delete all multidevs for a Pantheon site.
+#
+# Usage: ./pan-delete-multidevs.sh SITE
+
+set -eou pipefail
+
+site=${1}
+system_envs=("dev" "test" "live")
+all_envs="$(terminus env:list "$site" --field=ID)"
+
+echo "Deleting multidevs for ${site}..."
+
+for env in ${all_envs}
+do
+    if [[ ! " ${system_envs[*]} " =~ ${env} ]]; then
+        terminus multidev:delete "${site}"."${env}" --yes
+    fi
+done
