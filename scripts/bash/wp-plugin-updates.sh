b569445f26542640d13434d5cc16b06079217fae
Sarah German <sarah@sarahgerman.com>
2023-01-14 12:46:29 -0600

    Use Git mode for plugin updates
    SFTP mode is too unreliable.

--- scripts/bash/wp-plugin-updates.sh
+++ scripts/bash/wp-plugin-updates.sh
@@ -7,9 +7,7 @@
 
 set -eou pipefail
 
-# Number of seconds to pause between applying the update and trying to commit.
-# Pantheon takes a few seconds to register the code diffs as committable.
-OSD_SLEEP_INTERVAL=7
+UPDATED=()
 
 # Loop through all sites on the list.
 for SITENAME in ${SITES//,/ }; do
@@ -18,40 +16,47 @@ for SITENAME in ${SITES//,/ }; do
   PLUGINS="$(terminus wp "${SITENAME}".dev -- plugin update --all --format=json --dry-run </dev/null | jq -c '.[]')"
 
   if [ -n "$PLUGINS" ]; then
+    echo "$SITENAME has updates."
+    terminus connection:set "${SITENAME}".dev git
 
-    terminus connection:set "${SITENAME}".dev sftp
-    # @todo flag staged/uncommitted changes (e.g, leftovers from a script fail)
+    # Clone the site to a temporary directory.
+    TMP_DIR=$(mktemp -d 2>/dev/null || mktemp -d -t)
 
-    echo "$SITENAME has updates. Applying updates..."
+    GIT_URL="$(terminus connection:info "${SITENAME}".dev --field=git_url)"
+    git clone "${GIT_URL}" "${TMP_DIR}"/"${SITENAME}"
+    cd "${TMP_DIR}"/"${SITENAME}"
+
     for PLUGIN in $PLUGINS; do
       SLUG="$(echo "$PLUGIN" | jq -r '.name')"
       INSTALLED="$(echo "$PLUGIN" | jq -r '.version')"
       AVAILABLE="$(echo "$PLUGIN" | jq -r '.update_version')"
 
-      terminus wp "${SITENAME}".dev -- plugin update "${SLUG}" --format=summary </dev/null
+      rm -rf wp-content/plugins/"${SLUG}"
+      curl -O https://downloads.wordpress.org/plugin/"${SLUG}".zip
+      unzip "$SLUG".zip -d wp-content/plugins/
+      rm "$SLUG".zip
+
       MESSAGE="Update $SLUG ($INSTALLED => $AVAILABLE)."
-      sleep $OSD_SLEEP_INTERVAL
-      terminus env:commit "${SITENAME}".dev --message="${MESSAGE}"
+      git add . && git commit -am "${MESSAGE}"
+
+      UPDATED+=("${SITENAME}")
     done
+
+    # Push to Pantheon dev.
+    git push origin master
 
     # Apply DB updates on dev all at once.
     terminus wp "${SITENAME}".dev -- core update-db </dev/null
 
     # Deploy to Test env.
-    # @todo build a full update list and use it for the note
-    terminus env:deploy --sync-content --note="Maintenance updates." -- "${SITENAME}".test
+    terminus env:deploy --sync-content --note="Plugin updates." -- "${SITENAME}".test
     terminus wp "${SITENAME}".test -- core update-db </dev/null
     terminus env:clear-cache "${SITENAME}".test
-
-    # Put the dev site back in Git mode for added security.
-    terminus connection:set "${SITENAME}".dev git
-
   fi
 done
 
 # Updates are done, go check them out.
-# @todo handling for cases where there are no updates.
 echo "[notice] Go check the Test sites: "
-for SITENAME in ${SITES//,/ }; do
+for SITENAME in ${UPDATED//,/ }; do
   echo -e "- https://test-${SITENAME}.pantheonsite.io"
 done
